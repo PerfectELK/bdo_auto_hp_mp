94f0026710fb2de789a2d20a2b28d66991c318b0
Losev Andrey <Losikoban@Gmail.com>
2019-11-23 22:36:06 +0300

    console wariant is ready

--- main.py
+++ main.py
@@ -3,32 +3,64 @@ import time
 from PIL import ImageGrab
 import numpy as np
 import cv2
+import keyboard
+from threading import *
 
-#hsv_min = np.array((100, 100, 100), np.uint8)
-#hsv_max = np.array((255, 255, 255), np.uint8)
+run = [False]
 
-hsv_stamina_min = np.array((150, 100, 50), np.uint8)
+keys = []
+keys.append('f5')
+
+def listen(key, run):
+    while True:
+        keyboard.wait(key)
+        print(run)
+        if run[0] is False:
+            run[0] = True
+        else:
+            run[0] = False
+
+threads = [Thread(target=listen, kwargs={"key": key, "run": run}) for key in keys]
+for thread in threads:
+    thread.start()
+
+# hsv_min = np.array((100, 100, 100), np.uint8)
+# hsv_max = np.array((255, 255, 255), np.uint8)
+
+hsv_stamina_min = np.array((150, 100, 0), np.uint8)
 hsv_stamina_max = np.array((255, 255, 100), np.uint8)
 
-hsv_hp_min = np.array((100, 50, 30), np.uint8)
-hsv_hp_max = np.array((230, 100, 40), np.uint8)
+hsv_hp_min = np.array((100, 0, 0), np.uint8)
+hsv_hp_max = np.array((255, 60, 100), np.uint8)
 
 
 def select_color(min, max, image_array):
     return cv2.inRange(image_array, min, max)
-# 186 stamina
-# 430 hp num
+
+
+# 1700 stamina
+# 6000 hp num
 while True:
-    image = ImageGrab.grab(bbox=(750, 900, 1125, 1020))
-    image_np = np.array(image.getdata(), dtype='uint8').reshape(image.size[1], image.size[0], 3)
-    stamina = select_color(hsv_stamina_min, hsv_stamina_max, image_np)
-    hp = select_color(hsv_hp_min, hsv_hp_max, image_np)
-    staminaNum = cv2.countNonZero(stamina)
-    hpNum = cv2.countNonZero(hp)
-    if staminaNum <= 250:
-        PressKey(STAMINA)
-        ReleaseKey(STAMINA)
-    if hpNum <= 370:
-        PressKey(HEALTH)
-        ReleaseKey(HEALTH)
-    time.sleep(1.25)
+    while run[0]:
+        image = ImageGrab.grab(bbox=(145, 40, 455, 95))
+        # image.save('image.png')
+        image_np = np.array(image.getdata(), dtype='uint8').reshape(image.size[1], image.size[0], 3)
+        stamina = select_color(hsv_stamina_min, hsv_stamina_max, image_np)
+        hp = select_color(hsv_hp_min, hsv_hp_max, image_np)
+        # cv2.imwrite('hp.png', hp)
+        # cv2.imwrite('stam.png', stamina)
+        staminaNum = cv2.countNonZero(stamina)
+        hpNum = cv2.countNonZero(hp)
+
+        if staminaNum <= 1700:
+            PressKey(STAMINA)
+            ReleaseKey(STAMINA)
+        if hpNum <= 5900:
+            PressKey(HEALTH)
+            ReleaseKey(HEALTH)
+        time.sleep(.7)
+    if run[0] is False:
+        time.sleep(4)
+
+
+
